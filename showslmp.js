d8aba4f5997036e44d4a75a6ad34df52a4c9da80
buildthan <49547356+buildthan@users.noreply.github.com>
2022-06-15 08:25:13 +0900

    Add files via upload

--- showslmp.js
+++ showslmp.js
@@ -170,11 +170,13 @@ $( document )
   {  $(this).css({"fill" : "#FF850D"});
     //팝업 기능 잠시 봉인
     //document.querySelector('#pop_info_1').style.display = 'block';
+        placeid = $(this).text;
+
 		var sWidth = window.innerWidth;
 		var sHeight = window.innerHeight;
 
-		var oWidth = $('.popupLayer').width();
-		var oHeight = $('.popupLayer').height();
+		var oWidth = $('.' + placeid).width();
+		var oHeight = $('.' + placeid).height();
 
 		// 레이어가 나타날 위치를 셋팅한다.
 		var divLeft = e.clientX + 10;
@@ -188,7 +190,7 @@ $( document )
 		if( divLeft < 0 ) divLeft = 0;
 		if( divTop < 0 ) divTop = 0;
 
-		$('.popupLayer').css({
+		$('.' + placeid).css({
 			"top": divTop,
 			"left": divLeft,
 			"position": "absolute"
