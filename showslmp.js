1bc868ada9922c4247eb2ac17d88a30ff584d6a1
buildthan <49547356+buildthan@users.noreply.github.com>
2022-06-16 06:50:36 +0900

    Add files via upload

--- showslmp.js
+++ showslmp.js
@@ -10,8 +10,8 @@ var placeid = "";
 
 
 //맵을 만들기 전 구현 1단계 코드
-var seoulMapWidth = 600;
-var seoulMapHeight = 600;
+var seoulMapWidth = 1200;
+var seoulMapHeight = 1200;
 var initialScale = 65000;
 
 var initialX = seoulMapWidth/2+5;
