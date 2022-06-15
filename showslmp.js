2de93358846f8c3049c9f850af7a0c0aac164e1e
buildthan <49547356+buildthan@users.noreply.github.com>
2022-06-15 16:58:52 +0900

    Add files via upload

--- showslmp.js
+++ showslmp.js
@@ -127,7 +127,7 @@ d3.json("./seoul.json", function(error, data)
 
 //서울 구청 위치 정보 시각화
 
-/*
+
 d3.csv("placeseoul.csv", function(data)
 { placeMap.selectAll("circle") 
           .data(data).enter().append("circle") 
@@ -144,7 +144,7 @@ d3.csv("placeseoul.csv", function(data)
           .attr("id", function(d) { return d.seno+"name"; })
           .text(function(d) { return d.name; });
 });
-*/
+
 
 
 }
@@ -172,6 +172,9 @@ $( document )
     //document.querySelector('#pop_info_1').style.display = 'block';
 
     placeid = $(this).attr('class')
+    alert(placeid)
+    document.write(placeid)
+    console.log(placeid)
 
 
 		var sWidth = window.innerWidth;
