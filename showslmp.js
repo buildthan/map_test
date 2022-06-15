6f893d8ea7b114c39268ece0bd9d194e179486f5
buildthan <49547356+buildthan@users.noreply.github.com>
2022-06-15 16:37:37 +0900

    Add files via upload

--- showslmp.js
+++ showslmp.js
@@ -127,7 +127,7 @@ d3.json("./seoul.json", function(error, data)
 
 //서울 구청 위치 정보 시각화
 
-/*
+
 d3.csv("placeseoul.csv", function(data)
 { placeMap.selectAll("circle") 
           .data(data).enter().append("circle") 
@@ -144,7 +144,7 @@ d3.csv("placeseoul.csv", function(data)
           .attr("id", function(d) { return d.seno+"name"; })
           .text(function(d) { return d.name; });
 });
-*/
+
 
 
 }
@@ -170,13 +170,11 @@ $( document )
   {  $(this).css({"fill" : "#FF850D"});
     //팝업 기능 잠시 봉인
     //document.querySelector('#pop_info_1').style.display = 'block';
-    placeid = $(this).attr('class');
-
 		var sWidth = window.innerWidth;
 		var sHeight = window.innerHeight;
 
-		var oWidth = $('.'+ placeid).width();
-		var oHeight = $('.'+ placeid).height();
+		var oWidth = $('.popupLayer').width();
+		var oHeight = $('.popupLayer').height();
 
 		// 레이어가 나타날 위치를 셋팅한다.
 		var divLeft = e.clientX + 10;
@@ -190,7 +188,7 @@ $( document )
 		if( divLeft < 0 ) divLeft = 0;
 		if( divTop < 0 ) divTop = 0;
 
-		$('.'+ placeid).css({
+		$('.popupLayer').css({
 			"top": divTop,
 			"left": divLeft,
 			"position": "absolute"
@@ -210,8 +208,6 @@ $( document )
   })
 
 
-
-  /*
   // 팝업 열기
   for(var i = 0; i < target.length; i++){
     target[i].addEventListener('click', function(){
@@ -225,8 +221,6 @@ $( document )
     btnPopClose[j].addEventListener('click', function(){
       this.parentNode.parentNode.style.display = 'none';
     });
-  }
-
-  */
+}
 
 });
